622a8fd1d375ab23ce4fe4f6b13b5e635c5b235e
gus33000 <gus33000@live.fr>
2015-04-25 15:41:16 +0200

    Added Updater

---
+++ checkforupdates.bat
@@ -0,0 +1,60 @@
+@echo off
+for /f %%f in ('type version.txt') do set curver=%%f
+Echo [Info] Script Current Version : %curver%
+Echo [Info] Checking for Updates...
+set "url=https://raw.githubusercontent.com/gus33000/ESD-Decrypter/master/update.txt"
+if not exist "%temp%\ESD-Decrypter\update.txt" mkdir "%temp%\ESD-Decrypter"
+if exist "%temp%\ESD-Decrypter\update.txt" del "%temp%\ESD-Decrypter\update.txt"
+for /f "tokens=3 delims=:. " %%f in ('bitsadmin.exe /CREATE /DOWNLOAD "ESD-Decrypter Update Services" ^| findstr "Created job"') do set GUID=%%f
+bitsadmin.exe>nul /ADDFILE %GUID% %url% "%temp%\ESD-Decrypter\update.txt"
+bitsadmin.exe>nul /SETNOTIFYCMDLINE %GUID% "%SystemRoot%\system32\bitsadmin.exe" "%SystemRoot%\system32\bitsadmin.exe /COMPLETE %GUID%"
+bitsadmin.exe>nul /RESUME %GUID%
+
+:check
+bitsadmin /list | find "%GUID%" >nul 2>&1 && goto :check
+type %temp%\ESD-Decrypter\update.txt | find "%curver%" >nul 2>&1 && goto :uptodate
+for /f %%f in ('type %temp%\ESD-Decrypter\update.txt') do set NewVersion=%%f
+echo [Info] Found a new update for you : version %NewVersion%
+echo [Info] Downloading version %NewVersion%...
+set "url=http://gus33000.github.io/ESD-Decrypter/%NewVersion%.zip"
+for /f "tokens=3 delims=:. " %%f in ('bitsadmin.exe /CREATE /DOWNLOAD "ESD-Decrypter Update Services" ^| findstr "Created job"') do set GUID=%%f
+bitsadmin.exe>nul /ADDFILE %GUID% %url% "%temp%\ESD-Decrypter\%NewVersion%.zip"
+bitsadmin.exe>nul /SETNOTIFYCMDLINE %GUID% "%SystemRoot%\system32\bitsadmin.exe" "%SystemRoot%\system32\bitsadmin.exe /COMPLETE %GUID%"
+bitsadmin.exe>nul /RESUME %GUID%
+
+:check2
+bitsadmin /list | find "%GUID%" >nul 2>&1 && goto :check2
+echo [Info] Extracting version %NewVersion%...
+Call :UnZipFile "%temp%\ESD-Decrypter\%NewVersion%" "%temp%\ESD-Decrypter\%NewVersion%.zip"
+echo [Info] Applying update...
+xcopy "%temp%\ESD-Decrypter\%NewVersion%" "%CD%" /cheriky
+echo [Info] Update Applied : You are now up to date.
+pause
+exit /b
+
+
+:uptodate
+echo [Info] You are using the latest version of ESD-Decrypter !
+echo [Info] Running version %curver%
+pause
+exit /b
+
+:UnZipFile <ExtractTo> <newzipfile>
+set vbs="%temp%\_.vbs"
+if exist %vbs% del /f /q %vbs%
+>>%vbs% echo Set fso = CreateObject("Scripting.FileSystemObject")
+>>%vbs% echo ZipFile=fso.GetAbsolutePathName("%~2")
+>>%vbs% echo ExtractTo=fso.GetAbsolutePathName("%~1")
+>>%vbs% echo Set fso = CreateObject("Scripting.FileSystemObject")
+>>%vbs% echo If NOT fso.FolderExists(ExtractTo) Then
+>>%vbs% echo    fso.CreateFolder(ExtractTo)
+>>%vbs% echo End If
+>>%vbs% echo set objShell = CreateObject("shell.application")
+>>%vbs% echo set FilesInZip=objShell.NameSpace(ZipFile).items
+>>%vbs% echo objShell.NameSpace(ExtractTo).CopyHere(FilesInZip)
+>>%vbs% echo Set fso = Nothing
+>>%vbs% echo Set objShell = Nothing
+
+cscript //nologo %vbs%
+if exist %vbs% del /f /q %vbs%
+exit /b
