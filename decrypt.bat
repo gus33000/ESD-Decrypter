f0fe20d3ad8c7405c961e744e9fead7155fc31d9
gus33000 <gus33000@live.fr>
2015-04-29 13:50:37 +0200

    Bumped to 1016

--- decrypt.bat
+++ decrypt.bat
@@ -17,7 +17,7 @@ if "%1"=="/help" goto help
 :: UPDATE SYSTEM
 set "FILE=%~0"
 set "FILEN=%~nx0"
-set curver=1015
+set curver=1016
 call :updatesystem %*
 if "%~1"=="/noupdate" shift
 :: UPDATE SYSTEM
