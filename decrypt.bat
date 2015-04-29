960186207c8b8f4c1c5362c4eff71c3cdb3585b7
gus33000 <gus33000@live.fr>
2015-04-29 12:41:29 +0200

    Fixed newer support

--- decrypt.bat
+++ decrypt.bat
@@ -311,7 +311,7 @@ if %build% GEQ 9896 (
 	if /i %editionid%==Enterprise set DVDLABEL=J_CENA_%archl%FREV_%langid%_DV5&set DVDISO=%lang%_windows_10_enterprise_technical_preview_%build%_%arch%_dvd.iso
 )
 
-if %build% GEQ 10074 (
+if %build% GEQ 10066 (
 	set DVDLABEL=J_CCSA_%archl%FRE_%langid%_DV5&set DVDISO=%lang%_windows_10_insider_preview_%build%_%arch%_dvd.iso
 	if /i %editionid%==Core set DVDLABEL=J_CCRA_%archl%FRE_%langid%_DV5&set DVDISO=%lang%_windows_10_core_insider_preview_%build%_%arch%_dvd.iso
 	if /i %editionid%==CoreSingleLanguage set DVDLABEL=J_CSLA_%archl%FRER_%langid%_DV5&set DVDISO=%lang%_windows_10_singlelanguage_insider_preview_%build%_%arch%_dvd.iso
